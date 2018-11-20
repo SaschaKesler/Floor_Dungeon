51e03346ff9b1108282194c618b738471f003251
ak655140 <alexander.kesler@ima-ifu.rwth-aachen.de>
2018-11-20 14:28:19 +0100

    -created abilties (Skill and Spell class) -extended Character-class -moved packages to src package

---
+++ src/character/Character.cs
@@ -0,0 +1,22 @@
+public class Character {
+
+    //stats
+    private int health;
+    private int mana;
+    private int damage;
+    private int attackSpeed;
+    private int speed;
+    private int armor;
+    private int magicResistence;
+    
+    //ToDo implement equipment, ability (and inventory?) --> or make a new class equipment which contains all of these with inventory
+    private Weapon weapon; //maybe as list? or as array with length two (two as final static attribute)
+    private List<Armor> armor;
+    private List<Spell> spells;
+    private List<Skill> skills;
+
+    //others
+    private int level;
+    private int experience;
+    private int gold;
+}
